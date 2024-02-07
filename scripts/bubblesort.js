13dd00e59f7aefa41670de45b865fff8fcf91c56
Agnibo Chakraborty <agnibochakraborty97@gmail.com>
2024-02-08 04:21:57 +0530

    Working on the same

--- scripts/bubblesort.js
+++ scripts/bubblesort.js
@@ -1,6 +1,12 @@
-function bubbleSort() {
+// function bubbleSort() {
 
-    let arrays  = [1, 2, 3, 4,121];
-
-    for()
-};
+    let arrays  = [1, 5, 21, 2, 123, 211, 2, 112, 12, 331];
+    for(let j = 0; j < arrays.length - 1; j++) {
+      for(let i = 0; i < arrays.length - 1; i++) {
+        if (arrays[i] > arrays[i + 1]) {
+            [arrays[i], arrays[i+1]] = [arrays[i+1], arrays[i]];
+          }
+       }
+    }
+    console.log(arrays);
+// };
