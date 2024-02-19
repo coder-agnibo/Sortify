3cf721f36360109ab20403b50be72486be18749d
Agnibo <agnibochakraborty97@gmial.com>
2024-02-20 05:22:20 +0530

    Minnor Changes

---
+++ scripts/mergeSort.js
@@ -0,0 +1,16 @@
+async function mergeSort(){
+
+    let start = 0;
+    let end = bars.length-1;
+
+    let middle = Math.floor((start + end) /2);
+
+    await sort(bars, start, end);
+
+
+}
+
+
+async function sort(bars, start, end) {
+
+}
